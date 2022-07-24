6f989df2b106ee6c6ee3d7a9661b4ef6d566ea40
klashINkov <klash1200@gmail.com>
2022-07-25 00:52:22 +0300

    Mohammed issa in 21/7/2022 at 1:46 PM

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, deprecated_member_use, use_key_in_widget_constructors, avoid_print, camel_case_types, non_constant_identifier_names, must_be_immutable, unused_local_variable, unused_field
-
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
